1e4a840000bea19f7483d0cc18388d3f1086f107
teaualune <teaualune@gmail.com>
2018-05-24 14:31:29 +0800

    fix package name

--- device_linux.go
+++ device_linux.go
@@ -4,8 +4,8 @@ import (
 	"encoding/binary"
 	"net"
 
-	"github.com/bettercap/gatt/linux"
-	"github.com/bettercap/gatt/linux/cmd"
+	"github.com/teaualune/gatt/linux"
+	"github.com/teaualune/gatt/linux/cmd"
 )
 
 type device struct {
